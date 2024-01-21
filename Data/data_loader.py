bab96de9759ead97f64fc6fb89b020ca0cf39822
chinnl <nguyenngoclinhchi.23.12.2001@gmail.com>
2024-01-22 00:32:04 +0700

    "Dataloader infinite loop bug fixed

--- Data/data_loader.py
+++ Data/data_loader.py
@@ -13,13 +13,20 @@ from .common import ToIterableDataset, MapDataset
 
 class Cityscapes(Dataset):
     def __init__(self, 
-                 data_folder: str, #"D:\Data\Cityscapes\leftImg8bit\train",
+                 data_folder: str,
                  ):
         super().__init__()
         self.data_folder = data_folder
         image_list = glob.glob(data_folder + "/*/*")
         gt_dir = data_folder.replace("/images/", "/gtFine/")
-        
+        self.data_mapper = Dataset_mapper(is_train=True,
+                            use_instance_mask = True,
+                            augmentations=[RandomFlip(), 
+                                           ResizeShortestEdge([512], sample_style='choice')],
+                            image_format='BGR',
+                            instance_mask_format='bitmask',
+                            precomputed_proposal_topk=None,
+                            recompute_boxes=True)
         self.file_name = []
         self.sem_seg_file_name = []
         self.annotations = []
@@ -60,38 +67,40 @@ class Cityscapes(Dataset):
         return len(self.file_name)
     
     def __getitem__(self, idx):
-        return {
+        return self.data_mapper({
             'file_name': self.file_name[idx],
             'sem_seg_file_name': self.sem_seg_file_name[idx],
             'annotations': self.annotations[idx]
-        }
+        })
     
     
 def build_dataloader(cfg):
     train_folder = cfg.train.folder
     val_folder = cfg.val.folder
     
-    data_mapper = Dataset_mapper(is_train=True,
-                            use_instance_mask = True,
-                            augmentations=[RandomFlip(), 
-                                           ResizeShortestEdge([512], sample_style='choice')],
-                            image_format='BGR',
-                            instance_mask_format='bitmask',
-                            precomputed_proposal_topk=None,
-                            recompute_boxes=True)
+    # data_mapper = Dataset_mapper(is_train=True,
+    #                         use_instance_mask = True,
+    #                         augmentations=[RandomFlip(), 
+    #                                        ResizeShortestEdge([512], sample_style='choice')],
+    #                         image_format='BGR',
+    #                         instance_mask_format='bitmask',
+    #                         precomputed_proposal_topk=None,
+    #                         recompute_boxes=True)
+    cityscapes_train = Cityscapes(train_folder)
+    cityscapes_val = Cityscapes(val_folder)
     
-    cityscapes_train = MapDataset(Cityscapes(train_folder), data_mapper)
-    cityscapes_val = MapDataset(Cityscapes(val_folder), data_mapper)
+    # cityscapes_train = MapDataset(Cityscapes(train_folder), data_mapper)
+    # cityscapes_val = MapDataset(Cityscapes(val_folder), data_mapper)
 
-    train_sampler = TrainingSampler(len(cityscapes_train))
-    val_sampler = TrainingSampler(len(cityscapes_val))
+    # train_sampler = TrainingSampler(len(cityscapes_train))
+    # val_sampler = TrainingSampler(len(cityscapes_val))
     
-    cityscapes_train = ToIterableDataset(cityscapes_train, 
-                                         train_sampler, 
-                                         shard_chunk_size=cfg.train.batch_size)
-    cityscapes_val = ToIterableDataset(cityscapes_val, 
-                                         val_sampler, 
-                                         shard_chunk_size=cfg.val.batch_size)
+    # cityscapes_train = ToIterableDataset(cityscapes_train, 
+    #                                      train_sampler, 
+    #                                      shard_chunk_size=cfg.train.batch_size)
+    # cityscapes_val = ToIterableDataset(cityscapes_val, 
+    #                                      val_sampler, 
+    #                                      shard_chunk_size=cfg.val.batch_size)
     
     trainloader = DataLoader(cityscapes_train,
                             batch_size=cfg.train.batch_size,
