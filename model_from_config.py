175d823ab327379969d0d811275600f3f4b8b21f
chinnl <nguyenngoclinhchi.23.12.2001@gmail.com>
2024-02-17 15:42:44 +0000

    Minor changes

--- model_from_config.py
+++ model_from_config.py
@@ -1,7 +1,7 @@
 from Backbone import R50, FPN
 from Backbone.efficient_net import EfficientNet
 from RPN import RPN, RPN_Head, Anchor_Generator, Matcher, Box2BoxTransform
-from heads import Fast_RCNN_Conv_FC_Head, Fast_RCNN_Output_Layers, Mask_RCNN_Conv_Upsample_Head, Standard_ROI_Heads, ROI_Pooler, PanopticFPN, SemSeg_FPN_Head, RFCN
+from heads import *
 from torch import nn 
 import torch
 from layers import ShapeSpec
@@ -59,7 +59,7 @@ def load_model(cfg):
             box2box_transform = Box2BoxTransform(cfg.rpn.box_transform_weights), #equal scaling factors for all level
             box_reg_loss_type = cfg.rpn.box_reg_loss_type,
             )
-    load_rpn_weights(rpn)
+    # load_rpn_weights(rpn)
     
     box_pooler = ROI_Pooler(output_size = cfg.box_pooler.output_shape,
                             scales = cfg.box_pooler.scales,
@@ -135,182 +135,13 @@ def load_model(cfg):
     return model
 
 
-def load_panoptic_rfcn_model(cfg):
-    feature_shapes = {
-        k: ShapeSpec(channels=256, stride = v) 
-        for k, v in zip(["P2", "P3", "P4", "P5"], [4, 8, 16, 32, 64])
-    } 
-    bottom_up = R50()
-    load_pretrained_backbone(bottom_up)
-    dummy_data = torch.zeros((1,3,cfg.general.input_shape[0], cfg.general.input_shape[1]), dtype = torch.float32)
-    fpn = FPN(bottom_up(dummy_data))
-    backbone = Backbone(bottom_up, fpn)
-    
-    rpn_head = RPN_Head(4, len(cfg.anchor_generator.anchor_ratios))
-    anchor_gen = Anchor_Generator(cfg.anchor_generator.anchor_sizes, 
-                                    cfg.anchor_generator.anchor_ratios, 
-                                    cfg.anchor_generator.anchor_strides)
-       
-    rpn = RPN(head=rpn_head,
-            anchor_generator = anchor_gen,
-            anchor_matcher = Matcher(cfg.rpn.matcher.thresholds,
-                                    cfg.rpn.matcher.labels,
-                                    cfg.rpn.matcher.allow_low_quality_matches),
-            batch_size_per_image = cfg.rpn.batch_size_per_img,
-            positive_fraction = cfg.rpn.positive_fraction,
-            pre_nms_topk = cfg.rpn.pre_nms_topk,
-            post_nms_topk = cfg.rpn.post_nms_topk,
-            nms_thresh = cfg.rpn.nms_thresh,
-            loss_weight = cfg.rpn.loss_weights,
-            min_box_size = cfg.rpn.min_box_size,
-            box2box_transform = Box2BoxTransform(cfg.rpn.box_transform_weights), #equal scaling factors for all level
-            box_reg_loss_type = cfg.rpn.box_reg_loss_type,
-            )
-    
-    load_rpn_weights(rpn)
-    
-    mask_pooler = ROI_Pooler(output_size=cfg.mask_pooler.output_size,
-                            scales = cfg.mask_pooler.scales,
-                            sampling_ratio=cfg.mask_pooler.sampling_ratio,
-                            pooler_type=cfg.mask_pooler.pooler_type)
-    
-    mask_head = Mask_RCNN_Conv_Upsample_Head(input_shape = cfg.mask_head.input_shape,
-                                            num_classes = cfg.general.num_classes,
-                                            conv_dims = cfg.mask_head.conv_dims,
-                                            conv_norm = cfg.mask_head.conv_norm,
-                                            loss_weights = cfg.mask_head.loss_weight)
-    
-    rfcn_heads = RFCN(num_classes=cfg.general.num_classes,
-                        batch_size_per_image = cfg.roi_heads.batch_size_per_img,
-                        positive_fraction = cfg.roi_heads.positive_fraction,
-                        proposal_matcher = Matcher(thresholds=cfg.roi_heads.matcher.thresholds,
-                                                labels=cfg.roi_heads.matcher.labels,
-                                                allow_low_quality_matches=cfg.roi_heads.matcher.allow_low_quality_matches),
-                        box_in_features=cfg.roi_heads.box_in_features,
-                        output_size=cfg.box_pooler.output_shape,
-                        scales = cfg.box_pooler.scales,
-                        ignore_value = cfg.general.num_classes - 1,
-                        box2box_transform=Box2BoxTransform(cfg.box_predictor.box_transform_weights),
-                        box_reg_loss_type = 'smooth_l1',
-                        test_score_thresh=cfg.box_predictor.test_score_thresh,
-                        smooth_l1_beta=1.0,
-                        mask_pooler=mask_pooler,
-                        mask_head=mask_head,
-                        mask_in_features=cfg.roi_heads.mask_in_features,
-                        loss_weights = cfg.box_predictor.loss_weight,
-                        )
-    
-    sem_seg_head = SemSeg_FPN_Head( input_shape = feature_shapes,
-                                    num_classes = cfg.general.num_classes,
-                                    conv_dims = 128,
-                                    common_stride = 4,
-                                    loss_weight=cfg.semantic_head.loss_weight,
-                                    norm="GN", 
-                                    ignore_value = cfg.general.num_classes-1)
-    
-    pixel_mean = [72.5239, 83.0444, 73.2913] 
-    pixel_std = [0.1860, 0.1888, 0.1856] 
-    input_format="BGR"
-    
-    model = PanopticFPN(cfg = cfg,
-                        backbone = backbone,
-                        proposal_generator = rpn,
-                        roi_heads = rfcn_heads,
-                        sem_seg_head = sem_seg_head,
-                        combine_overlap_thresh = cfg.panoptic_head.combine_overlap_thresh,
-                        combine_stuff_area_thresh = cfg.panoptic_head.combine_stuff_area_thresh,
-                        combine_instances_score_thresh = cfg.panoptic_head.combine_instances_score_thresh,
-                        pixel_mean = pixel_mean,
-                        pixel_std = pixel_std,
-                        input_format = input_format)
-    print(f"Model is initialized with {int(num_params(model))} params. . . . .")
-    return model
 
 
-def load_panoptic_rfcn2_model(cfg):
-    feature_shapes = {
-        k: ShapeSpec(channels=256, stride = v) 
-        for k, v in zip(["P2", "P3", "P4", "P5"], [4, 8, 16, 32, 64])
-    } 
-    bottom_up = R50()
-    load_pretrained_backbone(bottom_up)
-    dummy_data = torch.zeros((1,3,cfg.general.input_shape[0], cfg.general.input_shape[1]), dtype = torch.float32)
-    fpn = FPN(bottom_up(dummy_data))
-    backbone = Backbone(bottom_up, fpn)
-    
-    rpn_head = RPN_Head(4, len(cfg.anchor_generator.anchor_ratios))
-    anchor_gen = Anchor_Generator(cfg.anchor_generator.anchor_sizes, 
-                                    cfg.anchor_generator.anchor_ratios, 
-                                    cfg.anchor_generator.anchor_strides)
-       
-    rpn = RPN(head=rpn_head,
-            anchor_generator = anchor_gen,
-            anchor_matcher = Matcher(cfg.rpn.matcher.thresholds,
-                                    cfg.rpn.matcher.labels,
-                                    cfg.rpn.matcher.allow_low_quality_matches),
-            batch_size_per_image = cfg.rpn.batch_size_per_img,
-            positive_fraction = cfg.rpn.positive_fraction,
-            pre_nms_topk = cfg.rpn.pre_nms_topk,
-            post_nms_topk = cfg.rpn.post_nms_topk,
-            nms_thresh = cfg.rpn.nms_thresh,
-            loss_weight = cfg.rpn.loss_weights,
-            min_box_size = cfg.rpn.min_box_size,
-            box2box_transform = Box2BoxTransform(cfg.rpn.box_transform_weights), #equal scaling factors for all level
-            box_reg_loss_type = cfg.rpn.box_reg_loss_type,
-            )
-    
-    load_rpn_weights(rpn)
-    
-    rfcn_heads = RFCN2(num_classes=cfg.general.num_classes,
-                        batch_size_per_image = cfg.roi_heads.batch_size_per_img,
-                        positive_fraction = cfg.roi_heads.positive_fraction,
-                        proposal_matcher = Matcher(thresholds=cfg.roi_heads.matcher.thresholds,
-                                                labels=cfg.roi_heads.matcher.labels,
-                                                allow_low_quality_matches=cfg.roi_heads.matcher.allow_low_quality_matches),
-                        box_in_features=cfg.roi_heads.box_in_features,
-                        output_size=cfg.box_pooler.output_shape,
-                        scales = cfg.box_pooler.scales,
-                        ignore_value = cfg.general.num_classes - 1,
-                        box2box_transform=Box2BoxTransform(cfg.box_predictor.box_transform_weights),
-                        box_reg_loss_type = 'smooth_l1',
-                        test_score_thresh=cfg.box_predictor.test_score_thresh,
-                        smooth_l1_beta=1.0,
-                        loss_weights = cfg.box_predictor.loss_weight,
-                        loss_mask_weights = cfg.mask_head.loss_weight
-                        )
-    
-    sem_seg_head = SemSeg_FPN_Head( input_shape = feature_shapes,
-                                    num_classes = cfg.general.num_classes,
-                                    conv_dims = 128,
-                                    common_stride = 4,
-                                    loss_weight=cfg.semantic_head.loss_weight,
-                                    norm="GN", 
-                                    ignore_value = cfg.general.num_classes-1)
-    
-    pixel_mean = [72.5239, 83.0444, 73.2913] 
-    pixel_std = [0.1860, 0.1888, 0.1856] 
-    input_format="BGR"
-    
-    model = PanopticFPN(cfg = cfg,
-                        backbone = backbone,
-                        proposal_generator = rpn,
-                        roi_heads = rfcn_heads,
-                        sem_seg_head = sem_seg_head,
-                        combine_overlap_thresh = cfg.panoptic_head.combine_overlap_thresh,
-                        combine_stuff_area_thresh = cfg.panoptic_head.combine_stuff_area_thresh,
-                        combine_instances_score_thresh = cfg.panoptic_head.combine_instances_score_thresh,
-                        pixel_mean = pixel_mean,
-                        pixel_std = pixel_std,
-                        input_format = input_format)
-    print(f"Model is initialized with {int(num_params(model))} params. . . . .")
-    return model
-
-
-def load_efficient_rfcn(cfg):
+def load_efficient_model(cfg):
     bottom_up = EfficientNet('efficientnet-b3')
     # load_pretrained_backbone(bottom_up)
     dummy_data = torch.zeros((1,3,cfg.general.input_shape[0], cfg.general.input_shape[1]), dtype = torch.float32)
-    fpn = FPN(bottom_up(dummy_data))
+    fpn = FPN(bottom_up(dummy_data), use_dwise_conv=True)
     backbone = Backbone(bottom_up, fpn)
     
     rpn_head = RPN_Head(4, len(cfg.anchor_generator.anchor_ratios))
@@ -334,38 +165,56 @@ def load_efficient_rfcn(cfg):
             box_reg_loss_type = cfg.rpn.box_reg_loss_type,
             )
     
-    load_rpn_weights(rpn)
+    # load_rpn_weights(rpn)
+    
+    box_pooler = ROI_Pooler(output_size = cfg.box_pooler.output_shape,
+                            scales = cfg.box_pooler.scales,
+                            sampling_ratio = cfg.box_pooler.sampling_ratio,
+                            pooler_type = cfg.box_pooler.pooler_type,
+                            )
+    
+    box_head = Fast_RCNN_Conv_AP_Head(input_shape = cfg.box_head.input_shape,
+                                    conv_dims = cfg.box_head.conv_dims,
+                                    fc_dims = cfg.box_head.fc_dims,
+                                    conv_norm = cfg.box_head.conv_norm)
+    
+    box_predictor = Fast_RCNN_Output_Layers(input_shape = cfg.box_predictor.input_shape,
+                                            test_score_thresh = cfg.box_predictor.test_score_thresh,
+                                            box2box_transform = Box2BoxTransform(cfg.box_predictor.box_transform_weights),
+                                            num_classes = cfg.general.num_thing_classes,
+                                            box_reg_loss_type = cfg.box_predictor.box_reg_loss_type,
+                                            smooth_l1_beta = cfg.box_predictor.smooth_l1_beta,
+                                            loss_weight = cfg.box_predictor.loss_weight,
+                                            # ignore_value = cfg.general.num_classes-1)
+                                            )
+    
     mask_pooler = ROI_Pooler(output_size=cfg.mask_pooler.output_size,
                             scales = cfg.mask_pooler.scales,
                             sampling_ratio=cfg.mask_pooler.sampling_ratio,
                             pooler_type=cfg.mask_pooler.pooler_type)
     
     mask_head = Mask_RCNN_Conv_Upsample_Head(input_shape = cfg.mask_head.input_shape,
-                                            num_classes = cfg.general.num_classes,
+                                            num_classes = cfg.general.num_thing_classes,
                                             conv_dims = cfg.mask_head.conv_dims,
                                             conv_norm = cfg.mask_head.conv_norm,
                                             loss_weights = cfg.mask_head.loss_weight,
                                             use_dwise_conv = True)
     
-    rfcn_heads = RFCN(num_classes=cfg.general.num_classes,
-                        batch_size_per_image = cfg.roi_heads.batch_size_per_img,
-                        positive_fraction = cfg.roi_heads.positive_fraction,
-                        proposal_matcher = Matcher(thresholds=cfg.roi_heads.matcher.thresholds,
-                                                labels=cfg.roi_heads.matcher.labels,
-                                                allow_low_quality_matches=cfg.roi_heads.matcher.allow_low_quality_matches),
-                        box_in_features=cfg.roi_heads.box_in_features,
-                        output_size=cfg.box_pooler.output_shape,
-                        scales = cfg.box_pooler.scales,
-                        ignore_value = cfg.general.num_classes - 1,
-                        box2box_transform=Box2BoxTransform(cfg.box_predictor.box_transform_weights),
-                        box_reg_loss_type = 'smooth_l1',
-                        test_score_thresh=cfg.box_predictor.test_score_thresh,
-                        smooth_l1_beta=1.0,
-                        mask_pooler=mask_pooler,
-                        mask_head=mask_head,
-                        mask_in_features=cfg.roi_heads.mask_in_features,
-                        loss_weights = cfg.box_predictor.loss_weight,
-                        )
+    roi_heads = Standard_ROI_Heads( num_classes = cfg.general.num_thing_classes,
+                                    batch_size_per_image = cfg.roi_heads.batch_size_per_img,
+                                    positive_fraction = cfg.roi_heads.positive_fraction,
+                                    proposal_matcher = Matcher(thresholds=cfg.roi_heads.matcher.thresholds,
+                                                                labels=cfg.roi_heads.matcher.labels,
+                                                                allow_low_quality_matches=cfg.roi_heads.matcher.allow_low_quality_matches),
+                                    box_in_features=cfg.roi_heads.box_in_features,
+                                    box_pooler=box_pooler,
+                                    box_head=box_head,
+                                    box_predictor=box_predictor,
+                                    mask_in_features=cfg.roi_heads.mask_in_features,
+                                    mask_pooler=mask_pooler,
+                                    mask_head=mask_head,
+                                    train_on_pred_boxes=cfg.roi_heads.train_on_pred_boxes
+                                    )
     
     feature_shapes = {
         k: ShapeSpec(channels=256, stride = v) 
@@ -386,7 +235,7 @@ def load_efficient_rfcn(cfg):
     model = PanopticFPN(cfg = cfg,
                         backbone = backbone,
                         proposal_generator = rpn,
-                        roi_heads = rfcn_heads,
+                        roi_heads = roi_heads,
                         sem_seg_head = sem_seg_head,
                         combine_overlap_thresh = cfg.panoptic_head.combine_overlap_thresh,
                         combine_stuff_area_thresh = cfg.panoptic_head.combine_stuff_area_thresh,
@@ -394,10 +243,9 @@ def load_efficient_rfcn(cfg):
                         pixel_mean = pixel_mean,
                         pixel_std = pixel_std,
                         input_format = input_format)
+    
     print(f"Model is initialized with {int(num_params(model))} params. . . . .")
     return model
-
-
 
 
 
